041f750c87848eaa635680f6a61cdfab1ecf9ee6
Alexi Luises <alexidev99@gmail.com>
2022-12-02 19:38:04 +0000

    deleted redundant code

--- day2/challenge2.ts
+++ day2/challenge2.ts
@@ -5,7 +5,7 @@ let challengeTwoScore: number = 0;
 var rps: [string, number, number][] = [["A X", 4, 3], ["B X", 1, 1], ["C X", 7, 2], ["A Y", 8, 4], ["B Y", 5, 5], ["C Y", 2, 6], ["A Z", 3, 8],["B Z", 9, 9],["C Z", 6, 7] ]
 
 const file = readFileSync('./rps.txt', 'utf-8').split(/\r?\n/);
-const rpsThing = file.map((index,i) => {
+file.map((index,i) => {
     rps.map((rpsindex,j)=>{
         switch (rps[j][0] == file[i]) {
             case true:
